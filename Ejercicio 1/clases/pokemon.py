464347b6a673707df1627340c0a3120283306b32
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-08 11:51:22 +0200

    init

---
+++ Ejercicio 1/clases/pokemon.py
@@ -0,0 +1,11 @@
+class pokemon:
+    def __init__(self,id,name,weapon,hp,dap,dep):
+        self.id=id
+        self.name=name
+        self.weapon=weapon
+        #puñetazo,patada,codazo,cabezazo
+        self.hp=hp
+        #health points
+        self.dap=dap
+        #damage points
+        self.dep=dep
