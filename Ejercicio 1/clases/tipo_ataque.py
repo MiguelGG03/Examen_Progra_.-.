1ea69a7123e77ada8f9c147ad8066940996be225
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-08 12:25:19 +0200

    clase ataque

--- Ejercicio 1/clases/tipo_ataque.py
+++ Ejercicio 1/clases/tipo_ataque.py
@@ -1,4 +1,6 @@
-class Ataque:
+from enum import Enum
+
+
+class Ataque(Enum):
     def __init__(self):
-        PUÑETAZO= Ataque.Enum
-        CABEZAZO= Ataque.Enum
+        #puñetazo(1),patada(2),codazo(3),cabezazo(4)
--- Ejercicio 1/main.py
+++ Ejercicio 1/main.py
@@ -1,4 +1,10 @@
 from clases.pokemon import *
 
+def main():
+    #Creamos a los entrenadores
+    entrenador1=[]
+    entrenador2=[]
+
+
 if __name__=='__main__':
-    
+    main()
