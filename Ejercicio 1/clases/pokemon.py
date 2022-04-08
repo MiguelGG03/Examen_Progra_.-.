106e3ddf59e1efcbab8bfe69da9a9363e8157e74
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-08 11:55:23 +0200

    algo de la clase pokemon

--- Ejercicio 1/clases/pokemon.py
+++ Ejercicio 1/clases/pokemon.py
@@ -1,11 +1,11 @@
 class pokemon:
-    def __init__(self,id,name,weapon,hp,dap,dep):
+    hp=0
+    #health points(1/100), inicialmente 0
+    def __init__(self,id,name,weapon,dap,dep):
         self.id=id
         self.name=name
         self.weapon=weapon
         #puñetazo(1),patada(2),codazo(3),cabezazo(4)
-        self.hp=hp
-        #health points(1/100)
         self.dap=dap
         #damage points(1/10)
         self.dep=dep
