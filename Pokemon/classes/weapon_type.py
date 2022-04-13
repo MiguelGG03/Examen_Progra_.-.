dd96f65199a1e7bf1c23832ecd67f56a00c1c826
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-13 12:38:27 +0200

    clase sacada de lo que hice en clase

--- Pokemon/classes/weapon_type.py
+++ Pokemon/classes/weapon_type.py
@@ -60,7 +60,30 @@ class WeaponType():
       >>> from weapon_type import WeaponType
       >>> obj_WeaponType = WeaponType.Boxer
     """
+    
+    PUÑETAZO = 2
+    PATADA = 4
+    CODAZO = 6
+    CABEZAZO = 10
 
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    
+    def tipo_ataque(stringdelarma):
+
+        stringdelarma=stringdelarma.lower()
+        if (stringdelarma == 'puñetazo'):
+            return Ataque.PUÑETAZO
+        elif (stringdelarma == 'patada'):
+            return Ataque.PATADA
+        elif (stringdelarma == 'codazo'):
+            return Ataque.CODAZO
+        elif (stringdelarma == 'cabezazo'):
+            return Ataque.CABEZAZO
+        else:
+            print('***'+str(stringdelarma.upper())+' NO ES UN TIPO DE ATAQUE REGISTRADO***')
 
 
 def main():
