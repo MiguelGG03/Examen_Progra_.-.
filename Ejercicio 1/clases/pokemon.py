18a94c8a2e5663cff423534f3d345c9d5a9c2a9a
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-13 12:46:35 +0200

    cambios en el original

--- Ejercicio 1/clases/pokemon.py
+++ Ejercicio 1/clases/pokemon.py
@@ -4,17 +4,50 @@ ids_pokemons=[]
 
 class pokemon:
     
-    def __init__(self,id,name,weapon,hp,DAP,DEP):
-        self.id=id
-        self.name=name
-        self.weapon=weapon
-        #puñetazo(1),patada(2),codazo(3),cabezazo(4)
-        self.hp=hp
-        #health points(1/100), inicialmente 0  
-        self.dap=DAP
-        #damage points(1/10)
-        self.dep=DEP
-        #defense points(1/10)
+    def __init__(self,id, pokemon_name, weapon_type, health_points,attack_rating, defense_rating):
+        if (isinstance(id,int)==True):
+            if(id not in pokemon.global_ids):
+                self.id=id
+                pokemon.global_ids.append(self.id)
+            else:
+                raise ValueError("El id del pokemon deberia ser uno nuevo id y no uno repetido de otro pokemon.")
+                # "raise" es para hacer saltar un error
+        else:
+            raise TypeError("El parametro id deberia ser un valor entero")
+
+        if(isinstance(pokemon_name,str)==True):
+            self.pokemon_name=pokemon_name
+        else:
+            raise TypeError("El nombre deberia ser un string")
+
+        if(isinstance(weapon_type,Ataque)==True):
+            self.weapon_type=weapon_type
+        else:
+            raise TypeError("El tipo de ataque no pertenece a la clase /Ataque/")
+                
+        if(isinstance(health_points,int)==True):
+            if(1<=health_points<=100):
+                self.health_points=health_points
+            else:
+                raise ValueError("La vida del poquemon debe estar entre 1 y 100")
+        else:
+            raise TypeError("La vida debe ser un valor entero")
+        
+        if(isinstance(attack_rating,int)==True):
+            if(1<=attack_rating<=100):
+                self.attack_rating=attack_rating
+            else:
+                raise ValueError("La vida del poquemon debe estar entre 1 y 100")
+        else:
+            raise TypeError("La vida debe ser un valor entero")
+        
+        if(isinstance(defense_rating,int)==True):
+            if(1<=defense_rating<=100):
+                self.defense_rating=defense_rating
+            else:
+                raise ValueError("La vida del poquemon debe estar entre 1 y 100")
+        else:
+            raise TypeError("La vida debe ser un valor entero")
 
     def elegir_ataque(self,pregunta):
         pregunta=input('Seleccione el ataque:\nPUÑETAZO \nPATADA \nCODAZO \nCABEZAZO\n-')
