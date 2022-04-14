cb8fd194895ea69922afaecee30d3abd7300d777
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-14 10:59:40 +0200

    metodo getdhadhash

--- Ejercicio 1/clases/pokemon.py
+++ Ejercicio 1/clases/pokemon.py
@@ -49,6 +49,17 @@ class pokemon:
         else:
             raise TypeError("La defensa debe ser un valor entero")
 
+
+    def set_defense_rating(self, defense_rating_to_be_set):
+        if isinstance((defense_rating_to_be_set, int)==True):
+            if (1 <= defense_rating_to_be_set <= 10):
+                self.defense_rating = defense_rating_to_be_set
+            else:
+                raise ValueError("El parametro defense_rating_to_be_set deberia estar entre 1 y 10")
+        else:
+            raise TypeError("El parametro defense_rating_to_be_set deberia ser un int")
+
+
     def leer_stats_pokemon(self):
         print("Pokemon ID"+str(self.id)+" cuyo nombre es "+self.pokemon_name+" tiene como ataque asignado"+
                 str(self.weapon_type.upper())+" y "+self.health_points+" puntos de vida.")
@@ -59,6 +70,7 @@ class pokemon:
         else:
             return False
 
+
     def fight_defense(self,points_of_damage):
         if(self.defense_rating<points_of_damage):
             self.health_points=self.health_points-(points_of_damage-self.defense_rating)
@@ -66,12 +78,14 @@ class pokemon:
         else:
             return False
 
+
     def fight_attack(self,pokemon_to_attack):
         if(pokemon_to_attack.fight_defense(self.attack_rating)==True):
             print("La nueva vida de "+pokemon_to_attack.pokemon_name+"\nes de "
                     +str(pokemon_to_attack.health_points)+" puntos de vida.")
         else:
             print("El ataque no ha sido efectivo")
+
 
     def elegir_ataque(self,pregunta):
         pregunta=input('Seleccione el ataque:\nPUÑETAZO \nPATADA \nCODAZO \nCABEZAZO\n-')
