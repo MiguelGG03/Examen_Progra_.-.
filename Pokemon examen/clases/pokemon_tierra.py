2d9e13ee0e563b30a5e12337902e40bd5ca11668
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-15 12:48:40 +0200

    creando la clase pokemon tierra pero con un minimo

--- Pokemon examen/clases/pokemon_tierra.py
+++ Pokemon examen/clases/pokemon_tierra.py
@@ -2,4 +2,8 @@ from clases.pokemon import pokemon
 from clases.tipo_ataque import Ataque
 
 class pokemonTierra(pokemon):
-    def
+    def __init__(self, pokemon_id, pokemon_name, weapon_type, health_points,
+                 attack_rating, defense_rating):
+        super().__init__(pokemon_id, pokemon_name, weapon_type, health_points,
+                 attack_rating, 10):
+                 
