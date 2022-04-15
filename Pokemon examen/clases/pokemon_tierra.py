7ae0f8f45a9b7bafac1fb43d4c94da60c06f3a4d
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-15 12:51:18 +0200

    el getter no cambia asi q lo voy a quitar :D

--- Pokemon examen/clases/pokemon_tierra.py
+++ Pokemon examen/clases/pokemon_tierra.py
@@ -2,8 +2,22 @@ from clases.pokemon import pokemon
 from clases.tipo_ataque import Ataque
 
 class pokemonTierra(pokemon):
+
+
     def __init__(self, pokemon_id, pokemon_name, weapon_type, health_points,
                  attack_rating, defense_rating):
+
+
         super().__init__(pokemon_id, pokemon_name, weapon_type, health_points,
-                 attack_rating, 10):
-                 
+                 attack_rating, 10)
+            
+        if(isinstance(defense_rating,int)==True):
+            if(11<=defense_rating<=20):
+                self.defense_rating=defense_rating
+            else:
+                raise ValueError("La defensa del pokemon debe estar entre 1 y 10")
+        else:
+            raise TypeError("La defensa debe ser un valor entero")
+    
+    def get_defense_rating(self):
+        return self.defense_rating
